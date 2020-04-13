3ed6ec81e453bb1d387ac5d79ee0af07e430f589
lurpis <hi@lurpis.com>
2020-04-13 16:20:08 +0800

    Add bifrost testnet node endpoints

--- packages/apps-config/src/settings/endpoints.ts
+++ packages/apps-config/src/settings/endpoints.ts
@@ -2,26 +2,34 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Option } from './types';
+import {Option} from './types';
 
 const DEV: Option[] = [
-  {
-    info: 'local',
-    text: 'Local Node (Own, 127.0.0.1:9944)',
-    value: 'ws://127.0.0.1:9944/'
-  }
+    {
+        info: 'local',
+        text: 'Local Node (Own, 127.0.0.1:9944)',
+        value: 'ws://127.0.0.1:9944/'
+    }
 ];
 
-const LIVE: Option[] = [
-
-];
+const LIVE: Option[] = [];
 
 const TEST: Option[] = [
-  {
-    info: 'bifrost',
-    text: 'Bifrost (Bifrost POC-2 Testnet, hosted by liebi)',
-    value: 'wss://testnet.liebi.com'
-  }
+    {
+        info: 'Bifrost Node 1',
+        text: 'Bifrost Node 1 (POC-2 Testnet, n1.testnet.liebi.com, hosted by Liebi)',
+        value: 'wss://n1.testnet.liebi.com'
+    },
+    {
+        info: 'Bifrost Node 2',
+        text: 'Bifrost Node 2 (POC-2 Testnet, n2.testnet.liebi.com, hosted by Liebi)',
+        value: 'wss://n2.testnet.liebi.com'
+    },
+    {
+        info: 'Bifrost Node 3',
+        text: 'Bifrost Node 3 (POC-2 Testnet, n3.testnet.liebi.com, hosted by Liebi)',
+        value: 'wss://n3.testnet.liebi.com'
+    }
 ];
 
 // The available endpoints that will show in the dropdown. For the most part (with the exception of
@@ -30,22 +38,22 @@ const TEST: Option[] = [
 //   text: The text to display on teh dropdown
 //   value: The actual hosted secure websocket endpoint
 export default [
-  {
-    isHeader: true,
-    text: 'Live networks',
-    value: ''
-  },
-  ...LIVE,
-  {
-    isHeader: true,
-    text: 'Test networks',
-    value: ''
-  },
-  ...TEST,
-  {
-    isHeader: true,
-    text: 'Development',
-    value: ''
-  },
-  ...DEV
-].map((option): Option => ({ ...option, withI18n: true }));
+    // {
+    //     isHeader: true,
+    //     text: 'Live networks',
+    //     value: ''
+    // },
+    // ...LIVE,
+    {
+        isHeader: true,
+        text: 'Test networks',
+        value: ''
+    },
+    ...TEST,
+    {
+        isHeader: true,
+        text: 'Development',
+        value: ''
+    },
+    ...DEV
+].map((option): Option => ({...option, withI18n: true}));
